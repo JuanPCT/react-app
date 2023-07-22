2221cd141cf3f99c27638994467b015aa7880967
Juan Pablo Correa Tarazona <89850618+JuanPCT@users.noreply.github.com>
2023-07-21 21:33:44 -0500

    project initialization

---
+++ src/main.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+
+const h1 = React.createElement('h1', null, 'Hello World');
+ReactDOM.createRoot(document.getElementById('root')).render(
+  h1
+  // <React.StrictMode>
+  //   <App />
+  // </React.StrictMode>,
+)
